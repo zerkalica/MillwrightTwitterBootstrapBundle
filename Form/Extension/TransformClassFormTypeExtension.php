7129247bbbafbc72f1082ff5cd21be832cd91939
Andrey Chernykh <a.d.mulberg@gmail.com>
2012-07-02 16:09:56 +0400

    Add new form extension

---
+++ Form/Extension/TransformClassFormTypeExtension.php
@@ -0,0 +1,23 @@
+<?php
+namespace Millwright\TwitterBootstrapBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TransformClassFormTypeExtension extends AbstractTypeExtension
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'transform_class' => null,
+        ));
+    }
+
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+}
